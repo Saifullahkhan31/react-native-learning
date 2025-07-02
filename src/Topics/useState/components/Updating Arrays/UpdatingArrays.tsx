b07081d9abf08547166a9c76f8abe2d0906fcf15
Saifullahkhan31 <saifullah.khan1203@gmail.com>
2025-07-02 07:04:37 +0500

    Learned useState function by updating arrays

--- src/Topics/useState/components/Updating Arrays/UpdatingArrays.tsx
+++ src/Topics/useState/components/Updating Arrays/UpdatingArrays.tsx
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Button } from 'react-native'
+import React, { useState } from 'react'
 
 const UpdatingArrays = () => {
+
+    const [friends, setfriends] = useState(['Alice', 'Bob', 'Charlie']);
+
+    const addFriend = () => setfriends([...friends, 'Saifullah'])
+
+    const removeFriend = () => setfriends(friends.filter(f => f !== 'Saifullah'))
+
+    const updateFriend = () => setfriends(friends.map(f => f == 'Saifullah' ? 'Saifullah Khan' : f))
+
   return (
     <View>
-      <Text>UpdatingArrays</Text>
+      
+    <FlatList
+    data={friends}
+    keyExtractor={(friend) => friend}
+    renderItem={({item}) => <Text>{item}</Text>}
+    />
+
+    <Button title='Add one friend' onPress={addFriend}/>
+
+    <Button title='Remove one friend' onPress={removeFriend}/>
+
+    <Button title='Update friend' onPress={updateFriend}/>
+
     </View>
   )
 }
